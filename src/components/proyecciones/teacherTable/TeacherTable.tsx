24737229b227f64922895b26eca1d95ca9b57cb8
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2025-02-04 09:11:56 -0400

    bugs fixxed

--- src/components/proyecciones/teacherTable/TeacherTable.tsx
+++ src/components/proyecciones/teacherTable/TeacherTable.tsx
@@ -1,21 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
-import type { TableColumnsType } from 'antd';
-import { Table, Tag, Input, Flex } from 'antd';
-import { MainContext } from '../../../context/mainContext';
-import { CloseCircleOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
-import { Teacher } from '../../../interfaces/teacher';
-import { MainContextValues } from '../../../interfaces/contextInterfaces';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import type { TableColumnsType } from "antd";
+import { Table, Tag, Input, Flex } from "antd";
+import { MainContext } from "../../../context/mainContext";
+import { CloseCircleOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
+import { Teacher } from "../../../interfaces/teacher";
+import { MainContextValues } from "../../../interfaces/contextInterfaces";
 
 const TeacherTable: React.FC = () => {
-
   const context = useContext(MainContext) as MainContextValues;
   const { teachers, setSelectedTeacherById, selectedQuarter } = context;
   const [data, setData] = useState<Teacher[] | null>([]);
-  const [searchText, setSearchText] = useState('');
-
-
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     if (!teachers) return;
@@ -26,128 +21,148 @@ const TeacherTable: React.FC = () => {
     if (!teachers) return;
 
     if (searchText.length === 0 && teachers) {
-      setData(teachers[selectedQuarter] || null)
-      return
+      setData(teachers[selectedQuarter] || null);
+      return;
     }
 
     const filteredTeachers = teachers[selectedQuarter]?.filter((teacher) => {
-      return teacher.name.toLowerCase().includes(searchText.toLowerCase()) ||
+      return (
+        teacher.name.toLowerCase().includes(searchText.toLowerCase()) ||
         teacher?.lastName?.toLowerCase().includes(searchText.toLowerCase()) ||
         teacher?.ci?.toLowerCase().includes(searchText.toLowerCase()) ||
         teacher.perfilName?.toLowerCase()?.includes(searchText.toLowerCase()) ||
-        teacher.type?.toLowerCase()?.includes(searchText.toLowerCase());
-    })
+        teacher.type?.toLowerCase()?.includes(searchText.toLowerCase())
+      );
+    });
     setData(filteredTeachers);
   }, [searchText, selectedQuarter, teachers]);
-
 
   const tagStyle: React.CSSProperties = {
     width: "100%",
     textAlign: "center",
-  }
-
-
+  };
 
   const columns: TableColumnsType<Teacher> = [
     {
-      title: 'Nombre',
-      dataIndex: 'name',
-      key: 'name',
-      width: '20vw',
+      title: "Nombre",
+      dataIndex: "name",
+      key: "name",
+      width: "20vw",
       sorter: (a, b) => a.name.localeCompare(b.name),
-      sortDirections: ['descend', 'ascend'],
-      render: (value) => <div >{value}</div>,
+      sortDirections: ["descend", "ascend"],
+      render: (value) => <div>{value}</div>,
     },
     {
-      title: 'Apellido',
-      dataIndex: 'lastName',
-      key: 'lastName',
-      width: '20vw',
+      title: "Apellido",
+      dataIndex: "lastName",
+      key: "lastName",
+      width: "20vw",
       sorter: (a, b) => a.lastName.localeCompare(b.lastName),
-      sortDirections: ['descend', 'ascend'],
-      render: (value) => <div >{value}</div>,
+      sortDirections: ["descend", "ascend"],
+      render: (value) => <div>{value}</div>,
     },
     {
-      title: 'Cédula',
-      dataIndex: 'ci',
-      key: 'ci',
-      width: '10vw',
+      title: "Cédula",
+      dataIndex: "ci",
+      key: "ci",
+      width: "10vw",
       sorter: (a, b) => Number.parseInt(a.ci) - Number.parseInt(b.ci),
-      sortDirections: ['descend', 'ascend'],
-      render: (value) => <div >{value}</div>,
+      sortDirections: ["descend", "ascend"],
+      render: (value) => <div>{value}</div>,
     },
     {
-      title: 'Tipo de contrato',
-      dataIndex: 'type',
-      key: 'type',
-      width: '10vw',
+      title: "Tipo de contrato",
+      dataIndex: "type",
+      key: "type",
+      width: "10vw",
       sorter: (a, b) => a.type.localeCompare(b.type),
-      sortDirections: ['descend', 'ascend'],
+      sortDirections: ["descend", "ascend"],
       render: (value) => {
         if (!value) {
-          return <Tag
-            color="red"
-            style={tagStyle}
-            icon={<ExclamationCircleOutlined />}
-            >
-            Sin contrato
-          </Tag>
+          return (
+            <Tag color="red" style={tagStyle} icon={<ExclamationCircleOutlined />}>
+              Sin contrato
+            </Tag>
+          );
         }
-        return <Tag color="blue" style={tagStyle}>{value}</Tag>
-      }
-
+        return (
+          <Tag color="blue" style={tagStyle}>
+            {value}
+          </Tag>
+        );
+      },
     },
     {
-      title: 'Horas Asignadas',
-      dataIndex: 'partTime',
+      title: "Horas Asignadas",
+      dataIndex: "partTime",
       render: (value, record) => {
         //no se puede usar el metodo desde el context, porque no funcionan los filtros
         const subjects = record.load ?? [];
-        const totalHours = subjects.reduce((acc, subject) => acc + subject.hours, 0);
+        const totalHours: number = subjects.reduce((acc, subject) => Number(acc) + Number(subject.hours), 0);
         let color = "black";
 
-        if (totalHours > value) {
+        if (totalHours > Number(value)) {
           color = "red";
         } else if (totalHours == 0) {
           color = "grey";
         }
 
-        if (!value) return <Tag color="warning" icon={<ExclamationCircleOutlined />} style={tagStyle}>{`Sin contrato`}</Tag>;
+        if (!value)
+          return (
+            <Tag color="warning" icon={<ExclamationCircleOutlined />} style={tagStyle}>{`Sin contrato`}</Tag>
+          );
 
-        return <div style={
-          {
-            textAlign: "center",
-            color
-          }
-        }>
-          {`${totalHours} / ${value}`}</div>;
+        return (
+          <div
+            style={{
+              textAlign: "center",
+              color,
+            }}>
+            {`${totalHours} / ${value}`}
+          </div>
+        );
       },
-      key: 'partTime',
-      width: '3vw',
+      key: "partTime",
+      width: "3vw",
       sorter: (a, b) => a.partTime - b.partTime,
-      sortDirections: ['descend', 'ascend'],
-    }
+      sortDirections: ["descend", "ascend"],
+    },
   ];
 
   const onRow = (record: Teacher) => {
-
     return {
       //onClick: () => { setSelectedTeacherById(Number.parseInt(record.id) - 1) },
-      onClick: () => { setSelectedTeacherById(record.id) },
+      onClick: () => {
+        setSelectedTeacherById(record.id);
+      },
     };
-  }
+  };
 
-
-
-  return <div style={{ width: "calc(100% - 40px)", height: "100%", cursor: "pointer", gridArea: "table" }} >
-    <Input style={{ width: "100%" }} placeholder="Buscar profesor" value={searchText} onChange={(e) => setSearchText(e.target.value)} allowClear/>
-    {
-    data?.length === 0 
-        ? <Tag icon={<CloseCircleOutlined />} color="error"  >Sin profesores encontrados</Tag>
-    :<Table pagination={{ position: ["topLeft", "none"] }} columns={columns} dataSource={data ?? []} rowKey="id" onRow={onRow} style={{ width: "100%", height: "100%", cursor: "pointer" }} />
-    }
-
-  </div>
+  return (
+    <div style={{ width: "calc(100% - 40px)", height: "100%", cursor: "pointer", gridArea: "table" }}>
+      <Input
+        style={{ width: "100%" }}
+        placeholder="Buscar profesor"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        allowClear
+      />
+      {data?.length === 0 ? (
+        <Tag icon={<CloseCircleOutlined />} color="error">
+          Sin profesores encontrados
+        </Tag>
+      ) : (
+        <Table
+          pagination={{ position: ["topLeft", "none"] }}
+          columns={columns}
+          dataSource={data ?? []}
+          rowKey="id"
+          onRow={onRow}
+          style={{ width: "100%", height: "100%", cursor: "pointer" }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default TeacherTable;
--- src/components/proyeccionesSubjects/proyeccionesSubjects.tsx
+++ src/components/proyeccionesSubjects/proyeccionesSubjects.tsx
@@ -1,22 +1,23 @@
-import "./proyeccionesSubjects.css"
-import { useContext, useEffect, useState } from "react"
-import { MainContext } from "../../context/mainContext"
-import { MainContextValues } from "../../interfaces/contextInterfaces"
-import { Radio, Button, Popconfirm, message, Tooltip } from 'antd';
-import type { RadioChangeEvent} from 'antd';
-import { QuestionCircleOutlined, DeleteOutlined } from '@ant-design/icons';
+import "./proyeccionesSubjects.css";
+import { useContext, useEffect, useState } from "react";
+import { MainContext } from "../../context/mainContext";
+import { MainContextValues } from "../../interfaces/contextInterfaces";
+import { Radio, Button, Popconfirm, message, Tooltip } from "antd";
+import type { RadioChangeEvent } from "antd";
+import { QuestionCircleOutlined, DeleteOutlined } from "@ant-design/icons";
 import TablePensum from "./table/table";
 import { Subject } from "../../interfaces/subject";
 import { GiAutoRepair } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
 
-
 export default function ProyeccionesSubjects() {
-  const { subjects, teachers, handleSubjectChange } = useContext(MainContext) as MainContextValues
+  const { subjects, teachers, handleSubjectChange, proyectionsDone } = useContext(
+    MainContext
+  ) as MainContextValues;
   const [errorRadio, setErrorRadio] = useState(true);
   const [asignedSubjects, setAsignedSubjects] = useState<Subject[]>([]);
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   const onChangeRadioError = (e: RadioChangeEvent) => {
     //console.log('radio checked', e.target.value);
     setErrorRadio(e.target.value);
@@ -24,84 +25,106 @@ export default function ProyeccionesSubjects() {
 
   //aqui se pasa el arreglo de materias que tienen los profesores asignadas
   useEffect(() => {
-    if(!teachers) return;
-    const asignedSubjectsQ1 = teachers.q1.map(teacher => teacher.load).flat();
-    const asignedSubjectsQ2 = teachers.q2.map(teacher => teacher.load).flat();
-    const asignedSubjectsQ3 = teachers.q3.map(teacher => teacher.load).flat();
+    if (!teachers) return;
+    const asignedSubjectsQ1 = teachers.q1.map((teacher) => teacher.load).flat();
+    const asignedSubjectsQ2 = teachers.q2.map((teacher) => teacher.load).flat();
+    const asignedSubjectsQ3 = teachers.q3.map((teacher) => teacher.load).flat();
     const uniqueAssignedSubjects = [
       ...new Map(
-        [
-          ...asignedSubjectsQ1,
-          ...asignedSubjectsQ2,
-          ...asignedSubjectsQ3
-        ].map(obj => [obj?.pensum_id, obj])
-      ).values()
+        [...asignedSubjectsQ1, ...asignedSubjectsQ2, ...asignedSubjectsQ3].map((obj) => [obj?.pensum_id, obj])
+      ).values(),
     ];
     setAsignedSubjects(uniqueAssignedSubjects as Subject[]);
-  }, [teachers])
- 
+  }, [teachers]);
+
   const handleRemoveErrorSubject = () => {
-    if(!subjects) return;
+    if (!subjects) return;
     const subjectsCopy = JSON.parse(JSON.stringify(subjects));
     //filtra que no tenga null
     const filteredSubjects = subjectsCopy.filter((subject: Subject) =>
-      Object.values(subject).every(value => value !== null)
+      Object.values(subject).every((value) => value !== null)
     );
     //filtra las horas
     const filteredHours = filteredSubjects.filter((subject: Subject) => subject.hours > 0);
     handleSubjectChange(filteredHours);
     message.success("Las materias con error fueron eliminadas de la proyección");
-  }
-  const iconStyle = { color: "white", fontSize: "2rem" }
+  };
+  const iconStyle = { color: "white", fontSize: "2rem" };
   // si no hay proyecciones
-  if (subjects?.length === 0) {
-    return <div className="proyecciones-container" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "20px" }}>
-      <h1>No se ha encontrado ninguna proyección</h1>
-      <Button
-        style={{ height: "60px", width: "300px", fontSize: "20px" }}
-        type="primary"
-        icon={<GiAutoRepair style={iconStyle} />}
-        onClick={() => navigate("/app/proyecciones/create")}>
-        Crear proyección
-      </Button>
-    </div>
+  if (subjects?.length === 0 && proyectionsDone?.length === 0) {
+    return (
+      <div
+        className="proyecciones-container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "20px",
+        }}>
+        <h1>No se ha encontrado ninguna proyección</h1>
+        <Button
+          style={{ height: "60px", width: "300px", fontSize: "20px" }}
+          type="primary"
+          icon={<GiAutoRepair style={iconStyle} />}
+          onClick={() => navigate("/app/proyecciones/create")}>
+          Crear proyección
+        </Button>
+      </div>
+    );
   }
 
+  return (
+    <>
+      <div
+        className="title-bar-container"
+        style={{
+          gridArea: "header",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "start",
+          columnGap: "3rem",
+        }}>
+        <h1>Materias en la Proyección</h1>
 
-  return (<>
-    <div className="title-bar-container" style={{ gridArea: "header", display: "flex", alignItems: "center", justifyContent: "start", columnGap: "3rem"}}>
-      <h1 >Materias en la Proyección</h1>
-    
-      <Radio.Group onChange={onChangeRadioError} value={errorRadio} >
-        <Radio value={true} style={{ color: "white" }}>Todas</Radio>
-        <Radio value={false} style={{ color: "white" }}>Error</Radio>
-      </Radio.Group>
-      <Popconfirm
-        title="¿Deseas borrar las materias con error?"
-        description="Esta operación no se puede deshacer"
-        onConfirm={handleRemoveErrorSubject}
-        onCancel={() => { }}
-        okText="Borrar Todas"
-        cancelText="Cancelar"
-        icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
-        okButtonProps={{ danger: true }}
-      >
-        <Tooltip placement="bottom" title={"Borrar TODAS las materias con error"} >
-          <Button type='link' danger shape="circle" icon={<DeleteOutlined style={{fontSize: "1.5rem"}} />} />
-        </Tooltip>
-      </Popconfirm>
-    </div>
+        <Radio.Group onChange={onChangeRadioError} value={errorRadio}>
+          <Radio value={true} style={{ color: "white" }}>
+            Todas
+          </Radio>
+          <Radio value={false} style={{ color: "white" }}>
+            Error
+          </Radio>
+        </Radio.Group>
+        <Popconfirm
+          title="¿Deseas borrar las materias con error?"
+          description="Esta operación no se puede deshacer"
+          onConfirm={handleRemoveErrorSubject}
+          onCancel={() => {}}
+          okText="Borrar Todas"
+          cancelText="Cancelar"
+          icon={<QuestionCircleOutlined style={{ color: "red" }} />}
+          okButtonProps={{ danger: true }}>
+          <Tooltip placement="bottom" title={"Borrar TODAS las materias con error"}>
+            <Button
+              type="link"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+          </Tooltip>
+        </Popconfirm>
+      </div>
 
-
-    <TablePensum subjects={
-      errorRadio
-        ? subjects?.concat(asignedSubjects)
-        : subjects?.concat(asignedSubjects)?.filter((subject) => {
-          return Object.values(subject).some(value => value === null || value === 0);
-        })
-    } />
-
-
-  </>
-  )
+      <TablePensum
+        subjects={
+          errorRadio
+            ? subjects?.concat(asignedSubjects)
+            : subjects?.concat(asignedSubjects)?.filter((subject) => {
+                return Object.values(subject).some((value) => value === null || value === 0);
+              })
+        }
+      />
+    </>
+  );
 }
+
