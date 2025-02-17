fb21c26ff2cb3713aadd2272a996e4c261c7f400
fj_rh@hotmail.com <javier@andinotechnologies.com>
2025-02-17 19:34:03 -0400

    Add ReportProyection component with print functionality and styling for report generation

--- src/components/reports/report/reportProyection.tsx
+++ src/components/reports/report/reportProyection.tsx
@@ -1,23 +1,37 @@
-import { useState, useContext, useEffect, useRef } from "react";
-import { Button, Modal, Select, Tag } from "antd";
+import React, { useState, useContext, useEffect, useRef, useMemo } from "react";
+import { Button, Modal, Select } from "antd";
 import { FaWpforms, FaPrint } from "react-icons/fa";
 import { MainContext } from "../../../context/mainContext";
 import { MainContextValues } from "../../../interfaces/contextInterfaces";
 import { Teacher } from "../../../interfaces/teacher";
 import { useReactToPrint } from 'react-to-print';
+import "./reportProyecion.css";
 
 interface SelectOption {
   label: string;
   value: string;
 }
 
+interface SubjectData {
+  teacherName: string;
+  teacherLastName: string;
+  teacherCi: string;
+  teacherContractType: string;
+  subject: string;
+  trayectoName: string;
+  seccion: string;
+  hours: string;
+  turnoName: string
+}
+
 const trayectoOpt: SelectOption[] = [
-  { label: "Trayecto 1", value: "1" },
-  { label: "Trayecto 2", value: "2" },
-  { label: "Trayecto 3", value: "3" },
+  { label: "Trimestre 1", value: "1" },
+  { label: "Trimestre 2", value: "2" },
+  { label: "Trimestre 3", value: "3" },
 ];
 
 const ReportProyection: React.FC = () => {
+  const rowCount = 17;
   const { pnfList, teachers } = useContext(MainContext) as MainContextValues;
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [pnfOptions, setPnfOptions] = useState<SelectOption[]>();
@@ -32,8 +46,123 @@ const ReportProyection: React.FC = () => {
   const handlePrint = useReactToPrint({
     contentRef: componentRef,
     documentTitle: "Reporte de proyecciones",
-
   });
+
+  const groupedData = useMemo(() => {
+    if (!reportData) return [];
+    const teacherMap = new Map<string, SubjectData[]>();
+
+    reportData.forEach(teacher => {
+      teacher.load?.forEach(subject => {
+        const entry = {
+          teacherName: teacher.name,
+          teacherLastName: teacher.lastName,
+          teacherCi: teacher.ci,
+          teacherContractType: teacher.type,
+          subject: subject.subject,
+          trayectoName: subject.trayectoName,
+          seccion: subject.seccion,
+          hours: subject.hours.toString(),
+          turnoName: subject.turnoName
+        };
+
+        if (teacherMap.has(teacher.name)) {
+          teacherMap.get(teacher.name)?.push(entry);
+        } else {
+          teacherMap.set(teacher.name, [entry]);
+        }
+      });
+    });
+
+    return Array.from(teacherMap, ([name, subjects]) => ({
+      name,
+      lastName: subjects[0]?.teacherLastName,
+      ci: subjects[0]?.teacherCi,
+      contractType: subjects[0]?.teacherContractType,
+      subjects
+    }));
+  }, [reportData]);
+
+
+  const chunks = useMemo(() => {
+    const result = [];
+    let currentChunk: SubjectData[] = [];
+    let currentCount = 0;
+
+    let teacherSubjects: SubjectData[] = [];
+
+    const addTeacherToChunk = () => {
+      if (teacherSubjects.length === 0) return;
+
+      // Verificar si el profesor cabe en el chunk actual
+      if (currentCount + teacherSubjects.length > rowCount) {
+        result.push(currentChunk);
+        currentChunk = [];
+        currentCount = 0;
+      }
+
+      currentChunk = currentChunk.concat(teacherSubjects);
+      currentCount += teacherSubjects.length;
+      teacherSubjects = [];
+    };
+
+    groupedData.forEach(teacherGroup => {
+      teacherSubjects = teacherGroup.subjects;
+
+
+      // Manejar caso donde un solo profesor excede el límite
+      if (teacherSubjects.length > rowCount) {
+        if (currentCount > 0) {
+          result.push(currentChunk);
+          currentChunk = [];
+          currentCount = 0;
+        }
+
+        // Dividir en chunks de rowCount
+        for (let i = 0; i < teacherSubjects.length; i += rowCount) {
+          result.push(teacherSubjects.slice(i, i + rowCount));
+        }
+        teacherSubjects = [];
+      } else {
+        addTeacherToChunk();
+      }
+    });
+
+    // Agregar los últimos elementos
+    if (currentChunk.length > 0) result.push(currentChunk);
+    if (teacherSubjects.length > 0) result.push(teacherSubjects);
+
+    return result;
+  }, [groupedData]);
+
+  const groupChunkByTeacher = (chunk: SubjectData[]) => {
+    const teacherMap = new Map<string, {
+      name: string;
+      lastName: string;
+      ci: string;
+      contractType: string;
+      subjects: SubjectData[];
+      turnoName: string
+    }>();
+
+    chunk.forEach(subject => {
+      if (teacherMap.has(subject.teacherName)) {
+        teacherMap.get(subject.teacherName)?.subjects.push(subject);
+      } else {
+        teacherMap.set(subject.teacherName, {
+          name: subject.teacherName,
+          lastName: subject.teacherLastName,
+          ci: subject.teacherCi,
+          contractType: subject.teacherContractType,
+          subjects: [subject],
+          turnoName: subject.turnoName
+        });
+      }
+    });
+
+    return Array.from(teacherMap.values());
+  };
+
 
   useEffect(() => {
     if (!pnfList) return;
@@ -60,7 +189,7 @@ const ReportProyection: React.FC = () => {
     setSelectedTrayecto(trayectoOpt[0].value);
   };
 
-  // se filtra por trayecto
+  // Filtrar por trayecto
   useEffect(() => {
     if (selectedTrayecto === "2") {
       setSelectedQuarter(teachers?.q2 || null);
@@ -74,12 +203,9 @@ const ReportProyection: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedTrayecto]);
 
-  // se filtra por pnf
+  // Filtrar por pnf
   useEffect(() => {
-    //se filtra que tengan carga academica
     const cleanTeachers = selectedQuarter?.filter((teacher) => teacher.load && teacher.load.length > 0);
-
-    //se filtra por pnf
     const cleanData = cleanTeachers?.filter((teacher) => {
       const load = teacher.load || [];
       return load.some((subject) => subject.pnfId === selectedPnf);
@@ -89,11 +215,11 @@ const ReportProyection: React.FC = () => {
   }, [selectedQuarter, selectedPnf]);
 
   const iconStyle = { color: "white", fontSize: "2rem" };
-  const border = "1px solid black";
+
+
   return (
     <>
       <Button type="link" shape="circle" icon={<FaWpforms />} onClick={showModal} style={iconStyle} />
-
 
       <Modal
         footer={null}
@@ -101,11 +227,11 @@ const ReportProyection: React.FC = () => {
         width="100vw"
         height="100vh"
         style={{ top: 0, left: 0, right: 0, bottom: 0 }}
-        title="Proyeciones"
+        title="Proyecciones"
         open={isModalOpen}
         onOk={handleOk}
-        onCancel={handleCancel}>
-
+        onCancel={handleCancel}
+      >
         <div>
           <div
             style={{
@@ -116,7 +242,8 @@ const ReportProyection: React.FC = () => {
               alignItems: "center",
               padding: "10px",
               borderRadius: "10px",
-            }}>
+            }}
+          >
             <div style={{ width: "100%", maxWidth: "400px" }}>
               <span style={{ color: "white" }}>Programa</span>
               <Select style={{ width: "100%" }} onChange={onChangePnf} options={pnfOptions} />
@@ -124,139 +251,86 @@ const ReportProyection: React.FC = () => {
 
             <div style={{ width: "100%", maxWidth: "400px" }}>
               <span style={{ color: "white" }}>Trayecto</span>
-              <Select
-                style={{ width: "100%" }}
-                onChange={onChageTrayecto}
-                value={selectedTrayecto}
-                options={trayectoOptions}
-              />
+              <Select style={{ width: "100%" }} onChange={onChageTrayecto} value={selectedTrayecto} options={trayectoOptions} />
             </div>
 
-            <Button disabled={!reportData || reportData.length === 0} type="link" shape="circle" icon={<FaPrint />} style={iconStyle} onClick={() => handlePrint()} />
-
-
+            <Button
+              disabled={!reportData || reportData.length === 0}
+              type="link"
+              shape="circle"
+              icon={<FaPrint />}
+              style={iconStyle}
+              onClick={() => handlePrint()}
+            />
           </div>
         </div>
-        {/* tabla*/}
 
-        <style>
-          {`
-              @media print {
-                @page {
-                  size: Letter;
-                  margin: 1cm;
-                  size: landscape; 
-                }
-                .header {
-                  position: fixed;
-                  top: 0;
-                  left: 0;
-                  right: 0;
-                  text-align: center;
-                  background: white;
-                  z-index: 1000;
-                  padding-bottom: 20px;
-                  background-color: white !important;
-                  width: 110%;
-                  height: 90px;
-           
-                }
-                .content {
-                 
-                }
-                .page-break {
-                  page-break-before: always;
-                  margin-bottom: 100px !important;
-                  background-color: red;
-                  width: 100%;
-                  height: 100px;
-                }
-                .teacher-row {
-                  page-break-inside: avoid;
-                }
-              }
-            `}
-        </style>
+        {/* Tabla */}
+     
 
-        <div style={{ 
+        <div
+          style={{
             width: "100%",
             height: "calc(100vh - 200px)",
-            overflow: "auto" 
-          }}>
-          <div className="report" ref={componentRef} >
-            <div className="header" style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-              <h3>PERSONAL DOCENTE</h3>
-              <h3>U.P.T. DE LOS LLANOS JUANA RAMIRÉZ, EXTENCIÓN ALTAGRACIA DE ORITUCO</h3>
-              <h3>{pnfOptions?.find((pnf) => pnf.value === selectedPnf)?.label.toUpperCase()} TRIMESTRE {selectedTrayecto}</h3>
-            </div>
+            overflow: "auto",
+          }}
+        >
+          <div className="report" ref={componentRef}>
+            {chunks.map((chunk, chunkIndex) => {
+              const teachersInChunk = groupChunkByTeacher(chunk);
 
-            <div className="content" style={{ marginTop: "100px" }}>
-              <div style={{
-                display: "grid",
-                gridTemplateColumns: "1fr 3fr",
-              }}>
-                <span style={{ border: border, textAlign: "center", fontWeight: "bold" }}>Docente</span>
-                <div style={{
-                  display: "grid",
-                  gridTemplateColumns: "3fr 3fr 1fr 1fr",
-                  borderBottom: border,
-                  borderTop: border,
-                  borderRight: border
-                }}>
-                  <span style={{  textAlign: "center", fontWeight: "bold" }}>Unidad Curricular</span>
-                  <span style={{ borderLeft: "1px solid black", textAlign: "center", fontWeight: "bold" }}>Trayecto</span>
-                  <span style={{ borderLeft: "1px solid black", textAlign: "center", fontWeight: "bold" }}>Seccion</span>
-                  <span style={{ borderLeft: "1px solid black", textAlign: "center", fontWeight: "bold" }}>Horas</span>
-                </div>
-              </div>
-              {
-                !reportData || reportData.length === 0
-                  ? <Tag color="warning" >{`No hay docentes con carga academica`}</Tag>
-                  : reportData.map((teacher: Teacher) => {
-                    const rowHeight = 1.2; // en cm
-                    // Altura total de las filas de load.map para este docente
-                    const totalLoadHeight = (teacher.load?.length || 0) * rowHeight;
-                    // Verificar si la altura acumulada supera el límite de la hoja (21.5cm para carta)
-                    const pageHeight = 21.5;
-                    const shouldBreak = totalLoadHeight > (pageHeight - rowHeight);
+              return (
+                <div key={chunkIndex} className={chunkIndex > 0 ? "page-break" : ""}>
+                  <Header
+                    selectedPnf={pnfOptions?.find(p => p.value === selectedPnf)?.label}
+                    selectedTrayecto={trayectoOptions.find(t => t.value === selectedTrayecto)?.label}
+                    page={chunkIndex}
+                  />
 
-                    return <div className="teacher-row" key={teacher.id} style={{ display: "grid", gridTemplateColumns: "1fr 3fr" }}>
-                      <div
-                        style={{
-                          borderRight: border,
-                          borderBottom: border,
-                          borderLeft: border,
-                          display: "flex",
-                          alignItems: "center",
-                          height: `${totalLoadHeight}cm`,
-                          paddingLeft: "10px",
-                        }}
-                      >
-                        {`${teacher.lastName} ${teacher.name}`}
-                      </div>
-                      <div style={{
-                        display: "grid",
-                        gridTemplateColumns: "3fr 3fr 1fr 1fr",
-                      }}>
-                        {
-                          teacher.load?.map((subject) => {
-                            return <>
-                              <div style={{ height: `${rowHeight}cm`, paddingLeft: "10px", borderRight: border, borderBottom: border, display: "flex", alignItems: "center" }}>{subject.subject}</div>
-                              <div style={{ height: `${rowHeight}cm`, paddingLeft: "10px", borderRight: border, borderBottom: border, display: "flex", alignItems: "center" }}>{subject.trayectoName}</div>
-                              <div style={{ height: `${rowHeight}cm`, paddingLeft: "10px", borderRight: border, borderBottom: border, display: "flex", alignItems: "center" }}>{`${subject.trayectoName[0]}-${subject.seccion}`}</div>
-                              <div style={{ height: `${rowHeight}cm`, paddingLeft: "10px", borderRight: border, borderBottom: border, display: "flex", alignItems: "center" }}>{subject.hours}</div>
+                  <table style={{ width: '100%', marginBottom: '20px' }}>
+                    <HeadRow />
+                    <tbody>
+                      {teachersInChunk.map((teacherGroup, groupIndex) => (
+                        teacherGroup.subjects.map((subject, subjectIndex) => (
+                          <tr key={`${groupIndex}-${subjectIndex}`}>
+                            {subjectIndex === 0 && 
+                            <>
+                              <td rowSpan={teacherGroup.subjects.length}>
+                                {`${teacherGroup.lastName} ${teacherGroup.name}`}
+                              </td>
+                              <td rowSpan={teacherGroup.subjects.length}>
+                                {subject.teacherCi}
+                              </td>
                             </>
-                          })
-                        }
-                      </div>
-
-                      {/* Forzar salto de página después de cada docente si es necesario */}
-                      {shouldBreak && <div className="page-break" />}</div>
-                  })
-
-              }
-            </div>
-          </div>
+                            }
+                            <td>{subject.subject}</td>
+                            <td>{subject.trayectoName}</td>
+                            <td>{`${subject.turnoName[0]}-0${subject.seccion}`}</td>
+                            <td>{subject.hours}</td>
+                            {subjectIndex === 0 && (
+                              <>
+                                <td rowSpan={teacherGroup.subjects.length}>
+                                  {teacherGroup.subjects.reduce((total, subject) =>
+                                    Number(total) + Number(subject.hours), 0
+                                  )}
+                                </td>
+                                <td rowSpan={teacherGroup.subjects.length}>
+                                  {teacherGroup.contractType}
+                                </td>
+                                <td rowSpan={teacherGroup.subjects.length}>
+                                  {/* Espacio para observaciones */}
+                                </td>
+                              </>
+                            )}
+                          </tr>
+                        ))
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              );
+            })}
+          </div>        
         </div>
       </Modal>
     </>
@@ -265,22 +339,35 @@ const ReportProyection: React.FC = () => {
 
 export default ReportProyection;
 
+interface HeaderProps {
+  selectedPnf?: string;
+  selectedTrayecto?: string;
+  page: number
+}
 
+function Header({ selectedPnf, selectedTrayecto, page }: HeaderProps) {
+  return (
+    <div className="header" style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+      <h3>PERSONAL DOCENTE</h3>
+      <h3>U.P.T. DE LOS LLANOS JUANA RAMÍREZ, EXTENCIÓN ALTAGRACIA DE ORITUCO</h3>
+      <h3>{selectedPnf?.toUpperCase()} -  {selectedTrayecto}</h3>
+      <h5>Pagina: {page + 1}</h5>
+    </div>
+  );
+}
 
-/*
- <style>
-        {`
-          @media print {
-            @page {
-              size: landscape; 
-              margin: 1cm; 
-            }
-            .report {
-              width: 100 %;
-              height: 100 %;
-            }
-          }
-        `}
-      </style>
-
-*/
+function HeadRow() {
+  return (
+    <tr>
+      <th>Nombre</th>
+      <th>C.I.</th>
+      <th>Unidad Curricular</th>
+      <th>Trayecto</th>
+      <th>Sección</th>
+      <th>Horas</th>
+      <th>Total Horas</th>
+      <th>Tipo de Contrato</th>
+      <th>Observación</th>
+    </tr>
+  );
+}
