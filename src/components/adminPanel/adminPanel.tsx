be8b945e689d46ccc4499fe23ef418e7d840971f
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2025-06-04 10:34:13 -0400

    feat: agregar funcionalidad para generar reportes en el panel de administración

--- src/components/adminPanel/adminPanel.tsx
+++ src/components/adminPanel/adminPanel.tsx
@@ -1,5 +1,9 @@
-import { Button, Divider } from "antd";
+import { Button, Divider, message, Radio, RadioChangeEvent, Select } from "antd";
 import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { MainContext } from "../../context/mainContext";
+import { MainContextValues } from "../../interfaces/contextInterfaces";
+import getReport from "../../fetch/report";
 
 const BUTTON_STYLE = {
   width: "200px",
@@ -15,17 +19,44 @@ const BUTTON_STYLE = {
   borderRadius: "4px",
   cursor: "pointer",
 };
+
+interface PnfMaskOptions {
+  value: string;
+  label: string;
+}
+
 export default function AdminPanel() {
   const navigate = useNavigate();
+  const { pnfList, userPNF } = useContext(MainContext) as MainContextValues;
+  const [pnfOptions, setPnfOptions] = useState<PnfMaskOptions[]>([]);
+  const [selectedPnf, setSelectedPnf] = useState<string | undefined>(undefined);
+  const [reportType, setReportType] = useState<number>(1);
+
+  useEffect(() => {
+    if (!pnfList) return;
+    const pnfOpt = pnfList.map((pnf) => ({
+      value: pnf.id.toString(),
+      label: pnf.name.toString(),
+    }));
+    setPnfOptions(pnfOpt);
+  }, [pnfList]);
 
   const handleCreateUserClick = () => {
-    // Puedes enviar un objeto con cualquier información que necesites
     navigate("/app/singin", { state: { redirect: "/app/admin" } });
   };
 
   const handleUpdateUserClick = () => {
-    // Puedes enviar un objeto con cualquier información que necesites
     navigate("/app/singin", { state: { redirect: "/app/admin", update: true } });
+  };
+
+  const handlePnfChange = (value: string) => {
+    setSelectedPnf(value);
+  };
+
+  const generateReport = async () => {
+    if (!selectedPnf || !reportType) return;
+    const report = await getReport({ pnfId: selectedPnf, type: reportType });
+    if (!report.success) return message.error(report.message, 5);
   };
 
   return (
@@ -41,8 +72,36 @@ export default function AdminPanel() {
           Actualizar Usuario
         </Button>
       </div>
+      <Divider orientation="left">Reportes</Divider>
+      <div style={{ display: "flex", paddingLeft: "20px", gap: "20px", alignItems: "center" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <span style={{ fontSize: "12px", color: "gray" }}>Programa</span>
+          <Select
+            defaultValue={userPNF}
+            style={{ width: 300 }}
+            onChange={handlePnfChange}
+            options={pnfOptions}
+            value={selectedPnf}
+          />
+        </div>
 
-      <Divider orientation="left">Mascara</Divider>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <span style={{ fontSize: "12px", color: "gray" }}>Tipo de Reporte</span>
+          <Select
+            defaultValue={reportType.toString()}
+            style={{ width: 300 }}
+            onChange={(value) => setReportType(Number.parseInt(value))}
+            options={[
+              { value: "1", label: "Trimestral" },
+              { value: "2", label: "Anual" },
+            ]}
+            value={reportType.toString()}
+          />
+        </div>
+        <Button style={BUTTON_STYLE} type="primary" onClick={generateReport}>
+          Generar
+        </Button>
+      </div>
     </div>
   );
 }
