d4ad88f7fb98c2c2f7de452f2c7c9b42b58e0e76
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2026-02-11 09:55:30 -0400

    xxxx

--- src/debug_subjects.ts
+++
@@ -1,4 +0,0 @@
-console.log('Debugging Subjects:', subjects);
-subjects?.forEach(s => {
-    console.log(`Subject: ${s.subject}, isSemestral: ${s.isSemestral}, Quarter Keys:`, Object.keys(s.quarter));
-});
